077f1d72f2b11362225b6d4ff5449875aab5d768
Huit Carlos <emox2544@gmail.com>
2018-11-27 14:31:26 -0600

    se configuran las variables de entorno de la api

--- server/config/index.js
+++ server/config/index.js
@@ -1,18 +1,12 @@
-export const secret = process.env.SECRET || 'mi clave secreta'
+const MONGODB_URI_SERVER = process.env.MONGODB_URI || 'mongodb://emox:mX179zAzAzA@ds241737.mlab.com:41737/welearn'
+const MONGODB_URI_LOCAL  = process.env.MONGODB_URI || 'mongodb://localhost/weduc'
+
+const PORT_SERVER = process.env.PORT || 8000
+const PORT_LOCAL  = process.env.PORT || 3000
+
+
 export const nameProject = 'WEDUC'
+export const secret      = process.env.SECRET || 'mi clave secreta'
+export const port        = process.env.NODE_ENV === 'production' ? PORT_SERVER        : PORT_LOCAL
+export const mongoURL    = process.env.NODE_ENV === 'production' ? MONGODB_URI_SERVER : MONGODB_URI_LOCAL
 
-
-
-if ( process.env.NODE_ENV === 'production' ) {
-  
-  export const port     = process.env.PORT || 8000
-  export const mongoURL = process.env.MONGODB_URI || 'mongodb://emox:mX179zAzAzA@ds241737.mlab.com:41737/welearn'
-  
-}
-
-if ( process.env.NODE_ENV === 'development' ) {
-
-  export const port     = process.env.PORT || 3000
-  export const mongoURL = process.env.MONGODB_URI || 'mongodb://localhost/weduc'
-  
-}
