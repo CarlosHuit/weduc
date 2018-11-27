f29a3fb362c5700c89322c773c5126dbc3370e36
Huit Carlos <emox2544@gmail.com>
2018-11-27 14:04:41 -0600

    se configuran las variables de entorno para produccion y desarrollo

--- server/config/index.js
+++ server/config/index.js
@@ -2,9 +2,17 @@ export const secret = process.env.SECRET || 'mi clave secreta'
 export const nameProject = 'WEDUC'
 
 
-// export const port = process.env.PORT || 3000
-// export const mongoURL = process.env.MONGODB_URI || 'mongodb://localhost/weduc'
 
+if ( process.env.NODE_ENV === 'production' ) {
+  
+  export const port     = process.env.PORT || 8000
+  export const mongoURL = process.env.MONGODB_URI || 'mongodb://emox:mX179zAzAzA@ds241737.mlab.com:41737/welearn'
+  
+}
 
-export const mongoURL = process.env.MONGODB_URI || 'mongodb://emox:mX179zAzAzA@ds241737.mlab.com:41737/welearn'
-export const port = process.env.PORT || 8000
+if ( process.env.NODE_ENV === 'development' ) {
+
+  export const port     = process.env.PORT || 3000
+  export const mongoURL = process.env.MONGODB_URI || 'mongodb://localhost/weduc'
+  
+}
