2f71fbca7060672917b997fafa0729c276eb16f1
Huit Carlos <emox2544@gmail.com>
2019-05-10 14:51:43 -0600

    add signup form class

---
+++ src/app/auth/models/signup-form.model.ts
@@ -0,0 +1,12 @@
+export class SignupForm {
+
+  constructor(
+    public email:     string,
+    public password:  string,
+    public password2: string,
+    public firstName: string,
+    public lastName:  string,
+    public avatar:    string,
+  ) {}
+
+}
