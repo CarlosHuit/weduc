a5e29872a3c3674129b770e8e4866fc98311fbb8
Huit Carlos <emox2544@gmail.com>
2019-05-10 14:45:07 -0600

    formatting

--- src/app/store/actions/courses.actions.ts
+++ src/app/store/actions/courses.actions.ts
@@ -9,31 +9,48 @@ export enum CoursesActionsType {
   RESET_DATA          = '[Courses] reset state'
 }
 
+
 export class GetCourses {
+
   static readonly type = CoursesActionsType.GET_COURSES;
+
 }
 
 
 export class GetCoursesSuccess {
+
   static readonly type = CoursesActionsType.GET_COURSES_SUCCESS;
   constructor(public payload: Course[]) {}
+
 }
+
 
 export class GetCourseSuccess {
+
   static readonly type = CoursesActionsType.GET_COURSE_SUCCESS;
   constructor(public payload: Course) {}
+
 }
+
 
 export class SelectCourse {
+
   static readonly type = CoursesActionsType.SELECT_COURSE;
   constructor(public payload: { course: string }) {}
+
 }
+
 
 export class IsLoadingCourses {
+
   static readonly type = CoursesActionsType.IS_LOADING_COURSES;
   constructor(public payload: {state: boolean}) {}
+
 }
 
+
 export class ResetCoursesData {
+
   static readonly type = CoursesActionsType.RESET_DATA;
+
 }
