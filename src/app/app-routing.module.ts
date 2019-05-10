794528c3b61a3045ac733b6c080783594edff489
Huit Carlos <emox2544@gmail.com>
2019-05-10 16:51:51 -0600

    prettify

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
-import { NgModule                 } from '@angular/core';
-import { Routes, RouterModule     } from '@angular/router';
-import { SigninComponent          } from './auth/signin/signin.component';
-import { SignupComponent          } from './auth/signup/signup.component';
-import { IsLoggedInGuard          } from './auth/is-logged-in.guard';
-import { AuthGuard                } from './auth/auth.guard';
-import { HomeComponent            } from './home/home.component';
-import { LettersMenuComponent     } from './read/letters-menu/letters-menu.component';
-import { LettersDetailComponent   } from './read/letters-detail/letters-detail.component';
-import { GameComponent            } from './read/game/game.component';
-import { DrawLetterComponent      } from './read/draw-letter/draw-letter.component';
-import { FindLetterComponent      } from './read/find-letter/find-letter.component';
-import { SelectWordsComponent     } from './read/select-words/select-words.component';
-import { PronounceLetterComponent } from './read/pronounce-letter/pronounce-letter.component';
-import { CourseDetailComponent    } from './course-detail/course-detail.component';
+import { NgModule                    } from '@angular/core';
+import { Routes, RouterModule        } from '@angular/router';
+import { AuthGuard                   } from './auth/auth.guard';
+import { HomeComponent               } from './home/home.component';
+import { GameComponent               } from './read/game/game.component';
+import { SigninComponent             } from './auth/signin/signin.component';
+import { SignupComponent             } from './auth/signup/signup.component';
+import { IsLoggedInGuard             } from './auth/is-logged-in.guard';
+import { DrawLetterComponent         } from './read/draw-letter/draw-letter.component';
+import { FindLetterComponent         } from './read/find-letter/find-letter.component';
+import { SelectWordsComponent        } from './read/select-words/select-words.component';
+import { LettersMenuComponent        } from './read/letters-menu/letters-menu.component';
+import { CourseDetailComponent       } from './course-detail/course-detail.component';
+import { LettersDetailComponent      } from './read/letters-detail/letters-detail.component';
+import { PronounceLetterComponent    } from './read/pronounce-letter/pronounce-letter.component';
 import { CheckDataReadingCourseGuard } from './check-data-reading-course.guard';
 
 
