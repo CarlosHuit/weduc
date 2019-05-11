ec1a20af3af9c4f1d24e3ac86ff75432007a8271
Huit Carlos <emox2544@gmail.com>
2019-05-11 14:35:59 -0600

    refactor code

--- src/app/shared/discussion-system/comment-thread/comment-thread.component.ts
+++ src/app/shared/discussion-system/comment-thread/comment-thread.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Comment } from '../../../models/discussion-system/comment.model';
+import { Select, Store } from '@ngxs/store';
+import { DiscussionSystemState } from 'src/app/store/state/discussion-system.state';
+import { Observable } from 'rxjs';
+import { ShowAnswersOf, HideAnswersOf, WriteAnswerFor } from 'src/app/store/actions/discussion-system.actions';
 
 @Component({
   selector: 'app-comment-thread',
@@ -7,9 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentThreadComponent implements OnInit {
 
-  constructor() { }
+  @Input() comment: Comment;
+
+  @Select(DiscussionSystemState.writeAnswerFor) writeAnswerFor$: Observable<string>;
+  @Select(DiscussionSystemState.showAnswers) showAnswers$: Observable<{}>;
+
+  constructor(private store: Store) { }
 
   ngOnInit() {
+  }
+
+  showAnswersOf(commentId: string) {
+
+    this.store.dispatch( new ShowAnswersOf( { commentId }));
+
+  }
+
+
+  hideAnswersOf(commentId: string) {
+
+    this.store.dispatch( new HideAnswersOf( { commentId }));
+
+  }
+
+
+  writeAnswerFor(commentId: string) {
+
+    this.store.dispatch( new WriteAnswerFor({ commentId }));
+
   }
 
 }
--- src/app/shared/discussion-system/discussion-system.component.ts
+++ src/app/shared/discussion-system/discussion-system.component.ts
@@ -2,13 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { Comment } from 'src/app/models/discussion-system/comment.model';
-import {
-  GetComments,
-  HideAnswersOf,
-  ResetDiscussionSystem,
-  ShowAnswersOf,
-  WriteAnswerFor
-} from '../../store/actions/discussion-system.actions';
+import { GetComments, ResetDiscussionSystem } from '../../store/actions/discussion-system.actions';
 import { DiscussionSystemState } from '../../store/state/discussion-system.state';
 
 
@@ -21,12 +15,13 @@ import { DiscussionSystemState } from '../../store/state/discussion-system.state
 
 export class DiscussionSystemComponent implements OnInit, OnDestroy {
 
-  @Select(DiscussionSystemState.isLoadingComments)  loadingComments$:     Observable<boolean>;
-  @Select(DiscussionSystemState.writeAnswerFor)   writeAnswerFor$:    Observable<string>;
-  @Select(DiscussionSystemState.showAnswers)  showAnswers$:        Observable<{}>;
-  @Select(DiscussionSystemState.comments)   comments$:        Observable<Comment[]>;
+
+  @Select(DiscussionSystemState.isLoadingComments)  loadingComments$: Observable<boolean>;
+  @Select(DiscussionSystemState.comments) comments$: Observable<Comment[]>;
+
 
   constructor(private store: Store) { }
+
 
   ngOnInit() {
 
@@ -41,25 +36,5 @@ export class DiscussionSystemComponent implements OnInit, OnDestroy {
 
   }
 
-
-  showAnswersOf(commentId: string) {
-
-    this.store.dispatch( new ShowAnswersOf( { commentId }));
-
-  }
-
-
-  hideAnswersOf(commentId: string) {
-
-    this.store.dispatch( new HideAnswersOf( { commentId }));
-
-  }
-
-
-  writeAnswerFor(commentId: string) {
-
-    this.store.dispatch( new WriteAnswerFor({ commentId }));
-
-  }
 
 }
