9f4b149795b4c20f0ce7e9882adfb97d636ac89d
Huit Carlos <emox2544@gmail.com>
2019-05-14 07:30:44 -0600

    show toolbar shadow on desktop version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit, HostListener, OnDestroy  } from '@angular/core';
-import { ChangeStateDrawer, CloseDrawer   } from './store/actions/drawer.actions';
-import { DetectMobile   } from './store/actions/app.actions';
-import { Title          } from '@angular/platform-browser';
-import { Select, Store  } from '@ngxs/store';
-import { Observable, Subscription     } from 'rxjs';
-import { AuthService    } from './auth/service/auth.service';
-import { AppState       } from './store/state/app.state';
-import { DrawerState    } from './store/state/drawer.state';
-import { AuthState      } from './store/state/auth.state';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
+import { ChangeStateDrawer, CloseDrawer } from './store/actions/drawer.actions';
+import { Observable, Subscription } from 'rxjs';
+import { Select, Store } from '@ngxs/store';
+import { DetectMobile } from './store/actions/app.actions';
+import { DrawerState } from './store/state/drawer.state';
+import { AuthState } from './store/state/auth.state';
+import { AppState } from './store/state/app.state';
 import { Logout } from './store/actions/auth.actions';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -23,51 +22,64 @@ export class AppComponent implements OnInit, OnDestroy {
   sub1: Subscription;
   sub2: Subscription;
 
-  @Select(AppState.getTitle)         title$:         Observable<string>;
-  @Select(AuthState.urlAvatar)       urlAvatat$:     Observable<string>;
-  @Select(AuthState.fullName)        fullName$:      Observable<string>;
-  @Select(AuthState.isLoggedIn)      isLoggedIn$:    Observable<boolean>;
-  @Select(AuthState.getEmail)        email$:         Observable<string>;
-  @Select(DrawerState.opened)        opened$:        Observable<boolean>;
-  @Select(DrawerState.urlBackground) urlBackground$: Observable<string>;
+  @Select(AppState.getTitle)    title$:      Observable<string>;
+  @Select(AuthState.getEmail)   email$:      Observable<string>;
+  @Select(DrawerState.opened)   opened$:     Observable<boolean>;
+  @Select(AppState.isMobile)    isMobile$:   Observable<boolean>;
+  @Select(AuthState.fullName)   fullName$:   Observable<string>;
+  @Select(AuthState.urlAvatar)  urlAvatat$:  Observable<string>;
+  @Select(AuthState.isLoggedIn) isLoggedIn$: Observable<boolean>;
+  @Select(DrawerState.urlBackground) urlBackground$:    Observable<string>;
+  @Select(AppState.queryMobileMatch) queryMobileMatch$: Observable<boolean>;
+
 
   constructor (
     private _titleService: Title,
-    private _auth:         AuthService,
     private store:         Store,
   ) { }
 
 
   ngOnInit() {
-    this.sub1 = this.title$.subscribe(title => this._titleService.setTitle(title));
-    this.sub2 = this.opened$.subscribe((status: boolean) => this.statusDrawer = status );
+
+    this.sub1 = this.title$.subscribe( title => this._titleService.setTitle(title) );
+    this.sub2 = this.opened$.subscribe( status => this.statusDrawer = status );
+
   }
 
 
   ngOnDestroy() {
+
     this.sub1.unsubscribe();
     this.sub2.unsubscribe();
+
   }
 
 
   @HostListener('window :resize') onResize() {
+
     this.store.dispatch(new DetectMobile());
+
   }
 
 
-  isLoggedIn = () =>  this._auth.isLoggedIn();
+  closeDrawer() {
+
+    this.store.dispatch( new CloseDrawer() );
+
+  }
 
 
-  closeDrawer = () => this.store.dispatch( new CloseDrawer() );
+  toogleDrawer($event: boolean) {
 
-
-  toogleDrawer = ($event: boolean) => {
     this.store.dispatch( new ChangeStateDrawer({status: !this.statusDrawer}) );
+
   }
+
 
   logoutAndClose = () => this.store.dispatch( [
-    this.closeDrawer(),
+    new CloseDrawer(),
     new Logout(),
   ])
+
 
 }
