a7eeb01136a2a0f380ed9796d78254369ea1dad8
Huit Carlos <emox2544@gmail.com>
2019-05-16 16:48:03 -0600

    implements  well done modal

--- src/app/shared/dialogs/well-done/well-done.component.ts
+++ src/app/shared/dialogs/well-done/well-done.component.ts
@@ -1,28 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { SpeechSynthesisService } from 'src/app/services/speech-synthesis.service';
+import { HideSuccessScreenLD } from 'src/app/store/actions/reading-course/reading-course-letter-detail.actions';
 
 @Component({
   selector: 'app-well-done',
   templateUrl: './well-done.component.html',
   styleUrls: ['./well-done.component.css']
 })
-export class WellDoneComponent implements OnInit {
+export class WellDoneComponent implements OnInit, OnDestroy {
 
   activeHideDialog = false;
+  utterance: SpeechSynthesisUtterance;
 
-  constructor() { }
+  constructor(
+    private store: Store,
+    private tts: SpeechSynthesisService
+  ) { }
 
   ngOnInit() {
 
-    // setTimeout(() => {
-    //   this.activeHideDialog = true;
-    // }, 2000);
-
-    setTimeout(() => {
-      console.log('Bien Hecho');
-    }, 600);
+    setTimeout(this.speakWellDone, 600);
 
   }
 
+  ngOnDestroy() {
+
+    this.utterance.removeEventListener( 'end', this.activeAnimationHideDialog );
+
+  }
+
+
+
+  speakWellDone(): void {
+
+
+    this.utterance = this.tts.speak('Bien Hecho', 0.90);
+    this.utterance.addEventListener( 'end', this.activeAnimationHideDialog );
+
+  }
+
+
+
+  activeAnimationHideDialog(): void {
+
+    this.activeHideDialog = true;
+    setTimeout(this.hideWellDoneDialog, 600);
+
+  }
+
+
+  hideWellDoneDialog(): void {
+
+    this.store.dispatch(new HideSuccessScreenLD());
+
+  }
 
 
 }
