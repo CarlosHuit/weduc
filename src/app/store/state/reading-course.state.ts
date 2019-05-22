bef816a72a926b4e93e047b8f48b8691d27ca513
Huit Carlos <emox2544@gmail.com>
2019-05-22 02:47:50 -0600

    fix reading course start redirection

--- src/app/store/state/reading-course.state.ts
+++ src/app/store/state/reading-course.state.ts
@@ -831,8 +831,8 @@ export class ReadingCourseState {
   redirectMenu({ dispatch }: StateContext<ReadingCourseStateModel>, { payload }: RedirectMenu) {
 
     const letter = payload.letter.toLowerCase();
-    // const url = `lectura/detalle-letra/${letter}`;
-    const url = `lectura/pronunciar-letra/${letter}`;
+    const url = `lectura/detalle-letra/${letter}`;
+    // const url = `lectura/pronunciar-letra/${letter}`;
     // ! TODO
 
     dispatch([
